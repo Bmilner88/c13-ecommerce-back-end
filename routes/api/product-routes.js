0f3432c054a42a6839aa64eb9036ed1e74d5acf3
Benny623 <bmilner88@gmail.com>
2022-04-20 12:17:00 -0600

    finished product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,23 @@ router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
-
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
   });
 });
 
@@ -21,11 +37,20 @@ router.get('/:id', (req, res) => {
       id: req.params.id
     },
     attributes: [
-
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
     ],
     include: [
       {
-
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
       }
     ]
   })
